8807550152840fff06b9d16cd14cfa15837e63f4
Nico Caprioli <nico.caprioli@gmail.com>
2018-06-05 19:25:33 +0200

    Routes Tree Structure
    * Changed minimum C++ requirement from C++11 to C++14
    * Added C++17 compliant subset of std::string_view, added in std namespace but not compiled if C++17 compiler support is detected
    * Moved from linear route search to tree one. It supports all possible combination of fixed, param, optional and splat fragments on the same level (priority in aforesaid order)

--- src/server/router.cc
+++ src/server/router.cc
@@ -25,7 +25,7 @@ Request::Request(
 bool
 Request::hasParam(std::string name) const {
     auto it = std::find_if(params_.begin(), params_.end(), [&](const TypedParam& param) {
-            return param.name() == name;
+            return param.name() == std::string_view(name.data(), name.length());
     });
 
     return it != std::end(params_);
@@ -34,7 +34,7 @@ Request::hasParam(std::string name) const {
 TypedParam
 Request::param(std::string name) const {
     auto it = std::find_if(params_.begin(), params_.end(), [&](const TypedParam& param) {
-            return param.name() == name;
+            return param.name() == std::string_view(name.data(), name.length());
     });
 
     if (it == std::end(params_)) {
@@ -58,153 +58,6 @@ Request::splat() const {
     return splats_;
 }
 
-Route::Fragment::Fragment(std::string value)
-{
-    if (value.empty())
-        throw std::runtime_error("Invalid empty fragment");
-
-    init(std::move(value));
-}
-
-bool
-Route::Fragment::match(const std::string& raw) const {
-    if (flags.hasFlag(Flag::Fixed)) {
-        return raw == value_;
-    }
-    else if (flags.hasFlag(Flag::Parameter) || flags.hasFlag(Flag::Splat)) {
-        return true;
-    }
-
-    return false;
-}
-
-bool
-Route::Fragment::match(const Fragment& other) const {
-    return match(other.value());
-}
-
-void
-Route::Fragment::init(std::string value) {
-    if (value[0] == ':')
-        flags.setFlag(Flag::Parameter);
-    else if (value[0] == '*') {
-        if (value.size() > 1)
-            throw std::runtime_error("Invalid splat parameter");
-        flags.setFlag(Flag::Splat);
-    }
-    else {
-        flags.setFlag(Flag::Fixed);
-    }
-
-    // Let's search for any '?'
-    auto pos = value.find('?');
-    if (pos != std::string::npos) {
-        if (value[0] != ':')
-            throw std::runtime_error("Only optional parameters are currently supported");
-
-        if (pos != value.size() - 1)
-            throw std::runtime_error("? should be at the end of the string");
-
-        value_ = value.substr(0, pos);
-        flags.setFlag(Flag::Optional);
-    } else {
-        value_ = std::move(value);
-    }
-
-    checkInvariant();
-}
-
-void
-Route::Fragment::checkInvariant() const {
-    auto check = [this](std::initializer_list<Flag> exclusiveFlags) {
-        for (auto flag: exclusiveFlags) {
-            if (!flags.hasFlag(flag)) return;
-        }
-
-        throw std::logic_error(
-                std::string("Invariant violated: invalid combination of flags for fragment ") + value_);
-    };
-
-    check({ Flag::Fixed, Flag::Optional });
-    check({ Flag::Fixed, Flag::Parameter });
-    check({ Flag::Splat, Flag::Fixed });
-    check({ Flag::Splat, Flag::Optional });
-    check({ Flag::Splat, Flag::Parameter });
-}
-
-std::vector<Route::Fragment>
-Route::Fragment::fromUrl(const std::string& url) {
-    std::vector<Route::Fragment> fragments;
-
-    std::istringstream iss(url);
-    std::string p;
-
-    while (std::getline(iss, p, '/')) {
-        if (p.empty()) continue;
-
-        fragments.push_back(Fragment(std::move(p)));
-    }
-
-    return fragments;
-}
-
-bool
-Route::Fragment::isParameter() const {
-    return flags.hasFlag(Flag::Parameter);
-}
-
-bool
-Route::Fragment::isOptional() const {
-    return isParameter() && flags.hasFlag(Flag::Optional);
-}
-
-bool
-Route::Fragment::isSplat() const {
-    return flags.hasFlag(Flag::Splat);
-}
-
-std::tuple<bool, std::vector<TypedParam>, std::vector<TypedParam>>
-Route::match(const Http::Request& req) const
-{
-    return match(req.resource());
-}
-
-std::tuple<bool, std::vector<TypedParam>, std::vector<TypedParam>>
-Route::match(const std::string& req) const
-{
-    static const auto NoMatch = std::make_tuple(false, std::vector<TypedParam>(), std::vector<TypedParam>());
-
-    auto reqFragments = Fragment::fromUrl(req);
-    if (reqFragments.size() > fragments_.size())
-        return NoMatch;
-
-    std::vector<TypedParam> params;
-    std::vector<TypedParam> splats;
-
-    for (std::vector<Fragment>::size_type i = 0; i < fragments_.size(); ++i) {
-        const auto& fragment = fragments_[i];
-        if (i >= reqFragments.size()) {
-            if (fragment.isOptional())
-                continue;
-
-            return NoMatch;
-        }
-
-        const auto& reqFragment = reqFragments[i];
-        if (!fragment.match(reqFragment))
-            return NoMatch;
-
-        if (fragment.isParameter()) {
-            params.push_back(TypedParam(fragment.value(), reqFragment.value()));
-        } else if (fragment.isSplat()) {
-            splats.push_back(TypedParam(reqFragment.value(), reqFragment.value()));
-        }
-
-    }
-
-    return make_tuple(true, std::move(params), std::move(splats));
-}
-
 namespace Private {
 
 RouterHandler::RouterHandler(const Rest::Router& router)
@@ -221,11 +74,220 @@ RouterHandler::onRequest(
     auto result = router.route(req, std::move(resp));
 
     /* @Feature: add support for a custom NotFound handler */
-    if (result == Router::Status::NotFound)
+    if (result == Route::Status::NotFound)
         response.send(Http::Code::Not_Found, "Could not find a matching route");
 }
 
 } // namespace Private
+
+FragmentTreeNode::FragmentTreeNode(): resourceRef_(), fixed_(), param_(), optional_(), splat_(), route_() {
+    std::shared_ptr<char> ptr(new char[1], std::default_delete<char[]>());
+    ptr.get()[0] = '/';
+    resourceRef_.swap(ptr);
+}
+
+FragmentTreeNode::FragmentTreeNode(const std::shared_ptr<char> &resourceReference) :
+        resourceRef_(resourceReference), fixed_(), param_(), optional_(), splat_(), route_() { }
+
+FragmentTreeNode::FragmentType
+FragmentTreeNode::getFragmentType(const std::string_view &fragment) {
+    // Let's search for any '?'
+    auto pos = fragment.find('?');
+    if (pos != std::string_view::npos) {
+        if (fragment[0] != ':') {
+            throw std::runtime_error("Only optional parameters are currently supported");
+        }
+        if (pos != fragment.length() - 1) {
+            throw std::runtime_error("? should be at the end of the string");
+        }
+        return FragmentType::Optional;
+    }
+
+    pos = fragment.find('*');
+    if (pos != std::string_view::npos && pos != 0) {
+        throw std::runtime_error("Invalid splat parameter");
+    }
+    if (fragment[0] == ':') {
+        return FragmentType::Param;
+    } else if (fragment[0] == '*') {
+        if (fragment.length() > 1) {
+            throw std::runtime_error("Invalid splat parameter");
+        }
+        return FragmentType::Splat;
+    }
+    return FragmentType::Fixed;
+}
+
+void
+FragmentTreeNode::addRoute(const std::string_view &path,
+                           Route::Handler &handler,
+                           std::shared_ptr<char> &resourceReference) {
+    if (path.length() == 0) {
+        throw std::runtime_error("Invalid zero-length URL.");
+    }
+    std::string_view currPath;
+    if (path.rfind('/') == path.length() - 1) {
+        currPath = path.substr(0, path.length() - 1);
+    } else {
+        currPath = path;
+    }
+    if (currPath.find('/') == std::string_view::npos) { // current leaf requested
+        if (route_ != nullptr) {
+            throw std::runtime_error("Requested route already exist.");
+        } else {
+            route_ = std::make_shared<Route>(handler);
+        }
+    } else { // recursion to correct descendant
+        const auto pos = currPath.find('/', 1);
+        const auto next = (pos == std::string_view::npos) ? currPath.substr(1) : currPath.substr(pos); // complete lower path
+        auto mid = (pos == std::string_view::npos) ? currPath.substr(1) : currPath.substr(1, currPath.find('/', pos) - 1); // middle resource name
+
+        std::unordered_map<std::string_view, std::shared_ptr<FragmentTreeNode>> *collection;
+        const auto fragmentType = getFragmentType(mid);
+        switch (fragmentType) {
+            case FragmentType::Fixed:
+                collection = &fixed_;
+                break;
+            case FragmentType::Param:
+                collection = &param_;
+                break;
+            case FragmentType::Optional:
+                mid = mid.substr(0, mid.length() - 1);
+                collection = &optional_;
+                break;
+            case FragmentType::Splat:
+                if (splat_ == nullptr) {
+                    splat_ = std::make_shared<FragmentTreeNode>(resourceReference);
+                }
+                splat_->addRoute(next, handler, resourceReference);
+                return;
+        }
+
+
+        if (collection->count(mid) == 0) { // direct subpath does not exist
+            collection->insert(std::make_pair(mid, std::make_shared<FragmentTreeNode>(resourceReference)));
+        }
+        collection->at(mid)->addRoute(next, handler, resourceReference);
+    }
+}
+
+bool Pistache::Rest::FragmentTreeNode::removeRoute(const std::string_view &path) {
+    if (path.length() == 0) {
+        throw std::runtime_error("Invalid zero-length URL.");
+    }
+    std::string_view currPath;
+    if (path.rfind('/') == path.length() - 1) {
+        currPath = path.substr(0, path.length());
+    } else {
+        currPath = path;
+    }
+    if (currPath.find('/') == std::string_view::npos) { // current leaf requested
+        route_.reset();
+    } else { // recursion to correct descendant
+        const auto pos = currPath.find('/', 1);
+        const auto next = (pos == std::string_view::npos) ? currPath.substr(1) : currPath.substr(pos); // complete lower path
+        auto mid = (pos == std::string_view::npos) ? currPath.substr(1) : currPath.substr(1, currPath.find('/', pos) - 1); // middle resource name
+        std::unordered_map<std::string_view, std::shared_ptr<FragmentTreeNode>> *collection;
+
+        auto fragmentType = getFragmentType(mid);
+        switch (fragmentType) {
+            case FragmentType::Fixed:
+                collection = &fixed_;
+                break;
+            case FragmentType::Param:
+                collection = &param_;
+                break;
+            case FragmentType::Optional:
+                mid = mid.substr(0, mid.length() - 1);
+                collection = &optional_;
+                break;
+            case FragmentType::Splat:
+                return splat_->removeRoute(next);
+        }
+
+        try {
+            const bool removable = collection->at(mid)->removeRoute(next);
+            if (removable) {
+                collection->erase(mid);
+            }
+        } catch (const std::out_of_range &) {
+            throw std::runtime_error("Requested does not exist.");
+        }
+    }
+    return fixed_.empty() && param_.empty() &&
+           optional_.empty() && splat_ == nullptr && route_ == nullptr;
+}
+
+Route::Status Pistache::Rest::FragmentTreeNode::invokeRouteHandler(
+        const std::string_view &path, const Http::Request &req,
+        Http::ResponseWriter response,
+        std::vector<TypedParam> &params,
+        std::vector<TypedParam> &splats) const {
+    if (path.length() == 0) {
+        throw std::runtime_error("Invalid zero-length URL.");
+    }
+    std::string_view currPath;
+    if (path.rfind('/') == path.length() - 1) {
+        currPath = path.substr(0, path.length() - 1);
+    } else {
+        currPath = path;
+    }
+    if (currPath.find('/') == std::string_view::npos) { // current leaf requested
+        route_->invokeHandler(Request(req, std::move(params), std::move(splats)), std::move(response));
+        return Route::Status::Match;
+    } else { // recursion to correct descendant
+        const auto pos = currPath.find('/', 1);
+        const auto next = (pos == std::string_view::npos) ? currPath.substr(1) : currPath.substr(pos); // complete lower path
+        auto mid = (pos == std::string_view::npos) ? currPath.substr(1) : currPath.substr(1, currPath.find('/', pos) - 1); // middle resource name
+
+        if (fixed_.count(mid) != 0) {
+            try {
+                return fixed_.at(mid)->invokeRouteHandler(next, req, std::move(response), params, splats);
+            } catch (std::runtime_error &) {
+
+            }
+        }
+
+        for (const auto &param: param_) {
+            try {
+                params.push_back(TypedParam(param.first, mid));
+                return param.second->invokeRouteHandler(next, req, std::move(response), params, splats);
+            } catch (std::runtime_error &) {
+                params.pop_back();
+            }
+
+        }
+
+        for (const auto &optional: optional_) {
+            try {
+                return optional.second->invokeRouteHandler(next, req, std::move(response), params, splats);
+            }
+            catch (std::runtime_error &) {}
+        }
+
+        try {
+            if (splat_!= nullptr) {
+                splats.push_back(TypedParam(mid, mid));
+                return splat_->invokeRouteHandler(next, req, std::move(response), params, splats);
+            }
+        } catch (std::runtime_error &) {
+            splats.pop_back();
+        }
+        throw std::runtime_error("Cannot find route on given leaf.");
+    }
+}
+
+Route::Status
+Pistache::Rest::FragmentTreeNode::invokeRouteHandler(const Http::Request& req, Http::ResponseWriter response) const {
+    std::vector<TypedParam> params;
+    std::vector<TypedParam> splats;
+    std::string_view path = {req.resource().data(), req.resource().length()};
+    try {
+        return invokeRouteHandler(path, req, std::move(response), params, splats);
+    } catch (std::runtime_error&) {
+        return Route::Status::NotFound;
+    }
+}
 
 Router
 Router::fromDescription(const Rest::Description& desc) {
@@ -287,33 +349,9 @@ Router::options(std::string resource, Route::Handler handler) {
 }
 
 void
-Router::clear_get(std::string resource) {
-    clearRoutes(Http::Method::Get, std::move(resource));
-}
-
-void
-Router::clear_post(std::string resource) {
-    clearRoutes(Http::Method::Post, std::move(resource));
-}
-
-void
-Router::clear_put(std::string resource) {
-    clearRoutes(Http::Method::Put, std::move(resource));
-}
-
-void
-Router::clear_patch(std::string resource) {
-    clearRoutes(Http::Method::Patch, std::move(resource));
-}
-
-void
-Router::clear_del(std::string resource) {
-    clearRoutes(Http::Method::Delete, std::move(resource));
-}
-
-void
-Router::clear_options(std::string resource) {
-    clearRoutes(Http::Method::Options, std::move(resource));
+Router::removeRoute(Http::Method method, std::string resource) {
+    auto& r = routes[method];
+    r.removeRoute(std::string_view(resource.data(), resource.length()));
 }
 
 void
@@ -321,48 +359,30 @@ Router::addCustomHandler(Route::Handler handler) {
     customHandlers.push_back(std::move(handler));
 }
 
-Router::Status
+Route::Status
 Router::route(const Http::Request& req, Http::ResponseWriter response) {
     auto& r = routes[req.method()];
-    for (const auto& route: r) {
-        bool match;
-        std::vector<TypedParam> params;
-        std::vector<TypedParam> splats;
-        std::tie(match, params, splats) = route.match(req);
-        if (match) {
-            route.invokeHandler(Request(req, std::move(params), std::move(splats)), std::move(response));
-            return Router::Status::Match;
+    try {
+        return r.invokeRouteHandler(req, std::move(response));
+    } catch (std::runtime_error&) {
+        for (const auto &handler: customHandlers) {
+            auto resp = response.clone();
+            auto result = handler(Request(req, std::vector<TypedParam>(), std::vector<TypedParam>()), std::move(resp));
+            if (result == Route::Result::Ok) return Route::Status::Match;
         }
-    }
 
-    for (const auto& handler: customHandlers) {
-        auto resp = response.clone();
-        auto result = handler(Request(req, std::vector<TypedParam>(), std::vector<TypedParam>()), std::move(resp));
-        if (result == Route::Result::Ok) return Router::Status::Match;
+        return Route::Status::NotFound;
     }
-
-    return Router::Status::NotFound;
 }
 
 void
 Router::addRoute(Http::Method method, std::string resource, Route::Handler handler)
 {
     auto& r = routes[method];
-    r.push_back(Route(std::move(resource), method, std::move(handler)));
+    std::shared_ptr<char> ptr(new char[resource.length()], std::default_delete<char[]>());
+    memcpy(ptr.get(), resource.data(), resource.length());
+    r.addRoute(std::string_view(ptr.get(), resource.length()), handler, ptr);
 }
-
-void Router::clearRoutes(Http::Method method, std::string resource) {
-    auto& r = routes[method];
-    for(int i = 0; i < r.size(); i++) {
-        if (resource == r[i].resource()) {
-            // move and erase, does not preserve order but has O(1) complexity
-            auto tmp = r[i];
-            r[i] = r[r.size() - 1];
-            r.pop_back();
-        }
-    }
-}
-
 
 namespace Routes {
 
@@ -390,28 +410,8 @@ void Options(Router& router, std::string resource, Route::Handler handler) {
     router.options(std::move(resource), std::move(handler));
 }
 
-void ClearGet(Router& router, std::string resource) {
-    router.clear_get(std::move(resource));
-}
-
-void ClearPost(Router& router, std::string resource) {
-    router.clear_post(std::move(resource));
-}
-
-void ClearPut(Router& router, std::string resource) {
-    router.clear_put(std::move(resource));
-}
-
-void ClearPatch(Router& router, std::string resource) {
-    router.clear_patch(std::move(resource));
-}
-
-void ClearDelete(Router& router, std::string resource) {
-    router.clear_del(std::move(resource));
-}
-
-void ClearOptions(Router& router, std::string resource) {
-    router.clear_options(std::move(resource));
+void Remove(Router& router, Http::Method method, std::string resource) {
+    router.removeRoute(method, resource);
 }
 
 } // namespace Routes
