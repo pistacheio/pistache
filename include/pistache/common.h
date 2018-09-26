79991168740adb393655bdbe915ddf64988336bb
Benoit Eudier <benoit.eudier@gmail.com>
2018-09-27 07:46:47 +0900

    Create a constant with default value for listener thread number

--- include/pistache/common.h
+++ include/pistache/common.h
@@ -59,6 +59,7 @@ namespace Const {
     static constexpr size_t MaxBacklog = 128;
     static constexpr size_t MaxEvents  = 1024;
     static constexpr size_t MaxBuffer  = 4096;
+    static constexpr size_t DefaultWorkers = 1;
 
     // Defined from CMakeLists.txt in project root
     static constexpr size_t DefaultMaxPayload = 4096;
