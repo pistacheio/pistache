df3e0c1476d538cd278bee461d2e0a081759680f
hasankandemir1993 <42869134+hasankandemir1993@users.noreply.github.com>
2018-10-22 01:40:54 +0300

    Update cookie.h

--- include/pistache/cookie.h
+++ include/pistache/cookie.h
@@ -1,6 +1,6 @@
-/* 
+/*
    Mathieu Stefani, 16 janvier 2016
-   
+
    Representation of a Cookie as per http://tools.ietf.org/html/rfc6265
 */
 
@@ -49,12 +49,15 @@ public:
     struct iterator : std::iterator<std::bidirectional_iterator_tag, Cookie> {
         iterator(const Storage::const_iterator& _iterator)
             : iter_storage(_iterator)
-        {             
-        }
-        
+            , iter_cookie_values()
+            , iter_storage_end()
+        { }
+
         iterator(const Storage::const_iterator& _iterator, const Storage::const_iterator& end)
-            : iter_storage(_iterator),iter_storage_end(end)
-        {   
+            : iter_storage(_iterator)
+            , iter_cookie_values()
+            , iter_storage_end(end)
+        {
             if(iter_storage != iter_storage_end) {
                 iter_cookie_values = iter_storage->second.begin();
             }
@@ -63,15 +66,19 @@ public:
         Cookie operator*() const {
             return iter_cookie_values->second; // return iter_storage->second;
         }
-        
-        iterator operator++() {
+
+        const Cookie* operator->() const {
+            return &(iter_cookie_values->second);
+        }
+
+        iterator& operator++() {
             ++iter_cookie_values;
             if(iter_cookie_values == iter_storage->second.end()) {
                 ++iter_storage;
                 if(iter_storage != iter_storage_end)
                     iter_cookie_values = iter_storage->second.begin();
             }
-            return iterator(iter_storage,iter_storage_end);
+            return *this;
         }
 
         iterator operator++(int) {
@@ -104,7 +111,7 @@ public:
 
     void add(const Cookie& cookie);
     void removeAllCookies();  
-    
+
     void addFromRaw(const char *str, size_t len);
     Cookie get(const std::string& name) const;
 
