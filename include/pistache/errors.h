7d72cacdd61ffa521a19211e164927de745cc98e
knowledge4igor <knowledge4igor@yahoo.com>
2018-11-08 00:09:19 +0300

    Improve error handling on accept call

---
+++ include/pistache/errors.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdexcept>
+
+namespace Pistache {
+namespace Tcp {
+
+class SocketError: public std::runtime_error
+{
+public:
+    explicit SocketError(const char* what_arg) : std::runtime_error(what_arg)
+    { }
+};
+
+class ServerError: public std::runtime_error
+{
+public:
+    explicit ServerError(const char* what_arg) : std::runtime_error(what_arg)
+    { }
+};
+
+} // Tcp
+} // Pistache
