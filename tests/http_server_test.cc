f1732e5e6099d293f8653148dda0d2ce56bbaa65
knowledge4igor <knowledge4igor@yahoo.com>
2018-11-11 12:54:20 +0300

    Revert "Comment one check in http_server_test (problem will be investigated). It seems there is a bug in server that makes this test unstable."
    This reverts commit 3872c4d8b2c6fefeffc54cfdcd2ec65b36178e5e.

--- tests/http_server_test.cc
+++ tests/http_server_test.cc
@@ -216,8 +216,7 @@ TEST(http_server_test, multiple_client_with_different_requests_to_multithreaded_
     if (hardware_concurrency() > 1)
     {
         ASSERT_EQ(res1, 0);
-        // TODO: Uncomment it after fixing bug in server
-        //ASSERT_EQ(res2, SECOND_CLIENT_REQUEST_SIZE);
+        ASSERT_EQ(res2, SECOND_CLIENT_REQUEST_SIZE);
     }
     else
     {
