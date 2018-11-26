cbc1a46366d2ef253d060e6cff801a609bb8e1ab
knowledge4igor <knowledge4igor@yahoo.com>
2018-11-27 01:44:32 +0300

    Tiny changes in test + delete std::cout

--- tests/cookie_test_3.cc
+++ tests/cookie_test_3.cc
@@ -1,3 +1,4 @@
+#include <pistache/async.h>
 #include <pistache/client.h>
 #include <pistache/cookie.h>
 #include <pistache/endpoint.h>
@@ -7,6 +8,7 @@
 
 #include <chrono>
 #include <unordered_map>
+#include <string>
 
 using namespace Pistache;
 
@@ -90,7 +92,6 @@ TEST(http_client_test, one_client_with_one_request_with_several_cookies) {
     response.then([&](Http::Response rsp) {
             for (auto&& cookie: rsp.cookies()) {
                 cookiesStorages[cookie.name] = cookie.value;
-                std::cout << "get: " << cookie.name << "\n";
             }
         },
         Async::IgnoreException);
