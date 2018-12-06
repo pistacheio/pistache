358bffdfbce71d75b3cecabf5b75050f46070ddb
knowledge4igor <knowledge4igor@yahoo.com>
2018-12-06 22:28:31 +0300

    Fix data races in http_client_test

--- tests/http_client_test.cc
+++ tests/http_client_test.cc
@@ -4,6 +4,7 @@
 
 #include "gtest/gtest.h"
 
+#include <atomic>
 #include <chrono>
 
 using namespace Pistache;
@@ -107,7 +108,7 @@ TEST(http_client_test, multiple_clients_with_one_request) {
     client3.init();
 
     std::vector<Async::Promise<Http::Response>> responses;
-    int response_counter = 0;
+    std::atomic<int> response_counter = 0;
 
     auto rb1 = client1.get(address);
     auto response1 = rb1.send();
