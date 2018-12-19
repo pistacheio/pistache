a8c09c80dcb2b7b6b17814fd4372685976297998
jcastro <jcastro@cray.com>
2018-12-19 14:33:44 +0000

    Add unit test to reproduce UB in Queue::Entry destructor
    See #379

---
+++ tests/mailbox_test.cc
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include <pistache/mailbox.h>
+
+struct Data {
+    static int num_instances;
+    static constexpr int fingerprint = 0xdeadbeef;
+
+    Data() : val(Data::fingerprint) {
+        num_instances++;
+    }
+
+    ~Data() {
+        EXPECT_EQ(val, Data::fingerprint);
+        EXPECT_GE(0, --num_instances );
+    }
+
+    int val;
+};
+
+int Data::num_instances = 0;
+constexpr int Data::fingerprint;
+
+TEST(queue_test, destructor_test) {
+    Pistache::Queue<Data> queue;
+    EXPECT_TRUE(queue.empty());
+
+    for( int i = 0; i < 5; i++ ) {
+        auto* e = queue.allocEntry(Data());
+        queue.push(e);
+    }
+    // Should call Data::~Data 5 times and not 6 (placeholder entry)
+}
+
