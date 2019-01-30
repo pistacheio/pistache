2dcb7587bf56947e4f6f64017a8d134786eea0ce
Arthur de Araújo Farias <arthur@afarias.org>
2019-01-30 23:33:48 +0100

    Fix memory leak in timers unordered map

--- src/common/transport.cc
+++ src/common/transport.cc
@@ -85,7 +85,7 @@ Transport::onReady(const Aio::FdSet& fds) {
                 auto it = timers.find(tag.value());
                 auto& entry = it->second;
                 handleTimer(std::move(entry));
-                timers.erase(it);
+                timers.erase(it->first);
             }
             else {
                 throw std::runtime_error("Unknown fd");
