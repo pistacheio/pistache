30e88eb4cd9ddd40430447103daecff33ddb2f25
Arthur de Araújo Farias <arthur@afarias.org>
2019-03-12 14:21:15 -0300

    Adding overcommit and pre-commit format hook to .overcommit.yml

---
+++ .githooks/pre-commit/format.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash -e
+
+#finding clang-format
+CLANG_FORMAT=${CLANG_FORMAT:-$(which clang-format)}
+
+#asserting clang format binnary
+if [ ! -x $CLANG_FORMAT ]; then
+    echo "Failed to format clang-format wasn't found."
+    exit -1
+fi
+
+#finding root directory
+ROOT_DIRECTORY=${ROOT_DIRECTORY:-$(realpath $(pwd))}
+
+# formatting all code
+clang-format -i -style=Mozilla $(find $ROOT_DIRECTORY -type d -name third-party* -prune -o -print | grep -E '.*\.(h|hpp|cc|cpp|cxx)$')
+
+# adding changes to commit
+git add $ROOT_DIRECTORY
