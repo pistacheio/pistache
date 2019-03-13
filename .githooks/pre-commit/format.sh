e95ee2dea2e4073a23a8b398557ada5aa3b3ba71
Arthur de Araújo Farias <arthur@afarias.org>
2019-03-12 21:53:09 -0300

    Adding OSS development requirements
    1 - pre-commit hook to check if code is formatted;
    2 - cmake custom target to format code;
    3 - build tests and build examples is now enabled by default when building as debug code;
    4 - format target is set to default when debug is selected;
    5 - if open source contributing is enabled valgrind, cppcheck, cmake-format are required.

--- .githooks/pre-commit/format.sh
+++ .githooks/pre-commit/format.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash -e
 
 #finding clang-format
@@ -11,10 +10,19 @@ if [ ! -x $CLANG_FORMAT ]; then
 fi
 
 #finding root directory
-ROOT_DIRECTORY=${ROOT_DIRECTORY:-$(realpath $(git rev-parse --show-cdup))}
+ROOT_DIRECTORY=${ROOT_DIRECTORY:-$(realpath ./$(git rev-parse --show-cdup))}
 
 # formatting all code
-clang-format -i -style=Mozilla $(find $ROOT_DIRECTORY -type d -name third-party* -prune -o -print | grep -E '.*\.(h|hpp|cc|cpp|cxx)$')
+REPLACEMENTS_COUNT=$(clang-format -i -style=Mozilla $(find $ROOT_DIRECTORY -type d -name third-party* -prune -o -print | grep -E '.*\.(h|hpp|cc|cpp|cxx)$') -output-replacements-xml | grep -c "<replacement " || true)
 
-# adding changes to commit
-git add $ROOT_DIRECTORY
+# show number of replacements found (unformatted code)
+echo "$REPLACEMENTS_COUNT format replacements found."
+
+# this scripts fails when there are replacements to be made and succeed when there are no replacements. This indicates that the code is formatted accordingly.
+if [[ $REPLACEMENTS_COUNT != 0 ]]; then
+    echo "This repository wasn't formatted. Please, format using clang-format before commit"
+    exit -1
+else
+    echo "Source code is correctly formatted"
+    exit 0
+fi
