bd80e0f4eb1f04ae1278b188d993c3189471fcf9
Tim Armstrong <timothy.armstrong@duxdigital.co.uk>
2019-05-21 14:54:06 +0100

    Fixes to tests and additional tests
    This should fix the broken tests
    * Adds address family validation to all `Address` tests
    * Adds direct tests to the IP class
    * Fixes tests broken by the clean-up

--- tests/net_test.cc
+++ tests/net_test.cc
@@ -30,65 +30,111 @@ TEST(net_test, address_creation)
 {
     Address address1("127.0.0.1:8080");
     ASSERT_EQ(address1.host(), "127.0.0.1");
+    ASSERT_EQ(address1.family(), AF_INET);
     ASSERT_EQ(address1.port(), 8080);
 
     std::string addr = "127.0.0.1";
     Address address2(addr, Port(8080));
     ASSERT_EQ(address2.host(), "127.0.0.1");
+    ASSERT_EQ(address2.family(), AF_INET);
     ASSERT_EQ(address2.port(), 8080);
 
     Address address3(Ipv4(127, 0, 0, 1), Port(8080));
     ASSERT_EQ(address3.host(), "127.0.0.1");
+    ASSERT_EQ(address3.family(), AF_INET);
     ASSERT_EQ(address3.port(), 8080);
 
     Address address4(Ipv4::any(), Port(8080));
     ASSERT_EQ(address4.host(), "0.0.0.0");
+    ASSERT_EQ(address4.family(), AF_INET);
     ASSERT_EQ(address4.port(), 8080);
 
     Address address5("*:8080");
     ASSERT_EQ(address5.host(), "0.0.0.0");
+    ASSERT_EQ(address5.family(), AF_INET);
     ASSERT_EQ(address5.port(), 8080);
 
     Address address6("[::1]:8080");
     ASSERT_EQ(address6.host(), "::1");
+    ASSERT_EQ(address6.family(), AF_INET6);
     ASSERT_EQ(address6.port(), 8080);
 
     std::string addr2 = "[::1]";
     Address address7(addr2, Port(8080));
     ASSERT_EQ(address7.host(), "::1");
+    ASSERT_EQ(address7.family(), AF_INET6);
     ASSERT_EQ(address7.port(), 8080);
 
     Address address8(Ipv6(0, 0, 0, 0, 0, 0, 0, 1), Port(8080));
     ASSERT_EQ(address8.host(), "::1");
+    ASSERT_EQ(address8.family(), AF_INET6);
     ASSERT_EQ(address8.port(), 8080);
 
-    Address address9(Ipv6::any(), Port(8080));
+    Address address9(Ipv6::any(true), Port(8080));
     ASSERT_EQ(address9.host(), "::");
+    ASSERT_EQ(address9.family(), AF_INET6);
     ASSERT_EQ(address9.port(), 8080);
 
     Address address10("[::]:8080");
     ASSERT_EQ(address10.host(), "::");
+    ASSERT_EQ(address10.family(), AF_INET6);
     ASSERT_EQ(address10.port(), 8080);
 
     Address address11("[2001:0DB8:AABB:CCDD:EEFF:0011:2233:4455]:8080");
     ASSERT_EQ(address11.host(), "2001:0DB8:AABB:CCDD:EEFF:0011:2233:4455");
+    ASSERT_EQ(address11.family(), AF_INET6);
     ASSERT_EQ(address11.port(), 8080);
 
     Address address12(Ipv4::loopback(), Port(8080));
     ASSERT_EQ(address12.host(), "127.0.0.1");
+    ASSERT_EQ(address12.family(), AF_INET);
     ASSERT_EQ(address12.port(), 8080);
 
-    Address address13(Ipv6::loopback(), Port(8080));
+    Address address13(Ipv6::loopback(true), Port(8080));
     ASSERT_EQ(address13.host(), "::1");
+    ASSERT_EQ(address13.family(), AF_INET6);
     ASSERT_EQ(address13.port(), 8080);
 
     Address address14("127.0.0.1");
     ASSERT_EQ(address14.host(), "127.0.0.1");
+    ASSERT_EQ(address14.family(), AF_INET);
     ASSERT_EQ(address14.port(), 80);
 
     Address address15("www.example.com");
     ASSERT_EQ(address15.host(), "93.184.216.34");
+    ASSERT_EQ(address15.family(), AF_INET);
     ASSERT_EQ(address15.port(), 80);
+    
+    Address address16(IP(127, 0, 0, 1), Port(8080));
+    ASSERT_EQ(address16.host(), "127.0.0.1");
+    ASSERT_EQ(address16.family(), AF_INET);
+    ASSERT_EQ(address16.port(), 8080);
+
+    Address address17(IP::any(), Port(8080));
+    ASSERT_EQ(address17.host(), "0.0.0.0");
+    ASSERT_EQ(address17.family(), AF_INET);
+    ASSERT_EQ(address17.port(), 8080);
+    
+    Address address18(IP(2, 0, 0, 0, 0, 0, 0, 1), Port(8080));
+    ASSERT_EQ(address18.host(), "2::1");
+    ASSERT_EQ(address18.family(), AF_INET6);
+    ASSERT_EQ(address18.port(), 8080);
+
+    Address address19(IP::any(true), Port(8080));
+    ASSERT_EQ(address19.host(), "::");
+    ASSERT_EQ(address19.family(), AF_INET6);
+    ASSERT_EQ(address19.port(), 8080);
+    
+    Address address20(IP::loopback(true), Port(8080));
+    ASSERT_EQ(address20.host(), "::1");
+    ASSERT_EQ(address20.family(), AF_INET6);
+    ASSERT_EQ(address20.port(), 8080);
+    
+    Address address21(IP::loopback(true), Port(8080));
+    ASSERT_EQ(address21.host(), "127.0.0.1");
+    ASSERT_EQ(address21.family(), AF_INET);
+    ASSERT_EQ(address21.port(), 8080);
+
 }
 
 TEST(net_test, invalid_address)
