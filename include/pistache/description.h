fecf880c7c30ee0835a94bc7f100b9ece23088d8
Dennis Jenkins <dennisjenkins@google.com>
2020-01-12 15:16:32 -0800

    Fixed cppcheck warnings for description.h

--- include/pistache/description.h
+++ include/pistache/description.h
@@ -140,7 +140,7 @@ struct Info {
 };
 
 struct InfoBuilder {
-    InfoBuilder(Info* info);
+    explicit InfoBuilder(Info* info);
 
     InfoBuilder& termsOfService(std::string value);
     InfoBuilder& contact(std::string name, std::string url, std::string email);
@@ -162,10 +162,16 @@ struct DataType {
 
 template<typename T>
 struct DataTypeT : public DataType {
-    const char* typeName() const { return Traits::DataTypeInfo<T>::typeName(); }
-    const char* format() const { return Traits::DataTypeInfo<T>::format(); }
+    const char* typeName() const override {
+      return Traits::DataTypeInfo<T>::typeName();
+    }
+    const char* format() const override {
+      return Traits::DataTypeInfo<T>::format();
+    }
 
-    bool validate(const std::string& input) const { return Traits::DataTypeValidation<T>::validate(input); }
+    bool validate(const std::string& input) const override {
+      return Traits::DataTypeValidation<T>::validate(input);
+    }
 
     virtual ~DataTypeT() {}
 };
@@ -290,7 +296,7 @@ private:
 };
 
 struct PathBuilder {
-    PathBuilder(Path* path);
+    explicit PathBuilder(Path* path);
 
     template<typename... Mimes>
     PathBuilder& produces(Mimes... mimes) {
@@ -447,7 +453,7 @@ private:
 
 class Swagger {
 public:
-    Swagger(const Description& description)
+    explicit Swagger(const Description& description)
         : description_(description)
         , uiPath_()
         , uiDirectory_()
