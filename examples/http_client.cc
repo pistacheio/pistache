73f248acd6db4c53e6604577b7e13fd5e756f96f
Igor [hyperxor] <56217938+hyperxor@users.noreply.github.com>
2020-03-29 17:53:47 +0300

    Issue #745: changes in http_client example (#746)

--- examples/http_client.cc
+++ examples/http_client.cc
@@ -6,64 +6,71 @@
 
 #include <atomic>
 
-#include <pistache/net.h>
-#include <pistache/http.h>
 #include <pistache/client.h>
+#include <pistache/http.h>
+#include <pistache/net.h>
 
 using namespace Pistache;
 using namespace Pistache::Http;
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: http_client page [count]" << std::endl;
-        return 1;
-    }
+  if (argc < 2) {
+    std::cerr << "Usage: http_client page [count]" << std::endl;
+    return 1;
+  }
 
-    std::string page = argv[1];
-    int count = 1;
-    if (argc == 3) {
-        count = std::stoi(argv[2]);
-    }
+  std::string page = argv[1];
+  int count = 1;
+  if (argc == 3) {
+    count = std::stoi(argv[2]);
+  }
 
-    Http::Client client;
+  Http::Client client;
 
-    auto opts = Http::Client::options()
-        .threads(1)
-        .maxConnectionsPerHost(8);
-    client.init(opts);
+  auto opts = Http::Client::options().threads(1).maxConnectionsPerHost(8);
+  client.init(opts);
 
-    std::vector<Async::Promise<Http::Response>> responses;
+  std::vector<Async::Promise<Http::Response>> responses;
 
-    std::atomic<size_t> completedRequests(0);
-    std::atomic<size_t> failedRequests(0);
+  std::atomic<size_t> completedRequests(0);
+  std::atomic<size_t> failedRequests(0);
 
-    auto start = std::chrono::system_clock::now();
+  auto start = std::chrono::system_clock::now();
 
-    for (int i = 0; i < count; ++i) {
-        auto resp = client.get(page).cookie(Http::Cookie("FOO", "bar")).send();
-        resp.then([&](Http::Response response) {
-                ++completedRequests;
-            std::cout << "Response code = " << response.code() << std::endl;
-            auto body = response.body();
-            if (!body.empty())
-               std::cout << "Response body = " << body << std::endl;
-        }, Async::IgnoreException);
-        responses.push_back(std::move(resp));
-        auto sync = Async::whenAll(responses.begin(), responses.end());
-        Async::Barrier<std::vector<Http::Response>> barrier(sync);
+  for (int i = 0; i < count; ++i) {
+    auto resp = client.get(page).cookie(Http::Cookie("FOO", "bar")).send();
+    resp.then(
+        [&](Http::Response response) {
+          ++completedRequests;
+          std::cout << "Response code = " << response.code() << std::endl;
+          auto body = response.body();
+          if (!body.empty())
+            std::cout << "Response body = " << body << std::endl;
+        },
+        [&](std::exception_ptr exc) {
+          ++failedRequests;
+          PrintException excPrinter;
+          excPrinter(exc);
+        });
+    responses.push_back(std::move(resp));
+  }
 
-        if(barrier.wait_for(std::chrono::seconds(5)) == std::cv_status::timeout) {
-                ++failedRequests;
-        }
-    }
+  auto sync = Async::whenAll(responses.begin(), responses.end());
+  Async::Barrier<std::vector<Http::Response>> barrier(sync);
+  barrier.wait_for(std::chrono::seconds(5));
 
-    auto end = std::chrono::system_clock::now();
-    std::cout << "Summary of execution" << std::endl
-              << "Total number of requests sent     : " << count << std::endl
-              << "Total number of responses received: " << completedRequests.load() << std::endl
-              << "Total number of requests failed   : " << failedRequests.load() << std::endl
-              << "Total time of execution           : "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+  auto end = std::chrono::system_clock::now();
+  std::cout << "Summary of execution" << std::endl
+            << "Total number of requests sent     : " << count << std::endl
+            << "Total number of responses received: "
+            << completedRequests.load() << std::endl
+            << "Total number of requests failed   : " << failedRequests.load()
+            << std::endl
+            << "Total time of execution           : "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                     start)
+                   .count()
+            << "ms" << std::endl;
 
-    client.shutdown();
+  client.shutdown();
 }
