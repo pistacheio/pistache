11288a27d811d7305b3beb6a32604c97e8a44fe0
Ian Roddis <gitlab@ie2r.com>
2020-11-17 16:14:49 -0400

    Reducing verbosity of test output

--- tests/http_server_test.cc
+++ tests/http_server_test.cc
@@ -438,8 +438,7 @@ struct ClientCountingHandler : public Http::Handler {
 
   void onDisconnection(const std::shared_ptr<Tcp::Peer> &peer) override {
     ++counter_;
-    std::cout << "[server] Disconnect from " << peer->address().host() << " now at " << counter_ << std::endl;
-    std::cout << "[server] Disconnect from " << peer->getID() << " now at " << std::endl;
+    std::cout << "[server] Disconnect from peer ID " << peer->getId() << " connecting from " << peer->address().host() << "; counter now at " << counter_ << std::endl;
     activeConnections.erase(peer->getID());
   }
 
