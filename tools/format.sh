e41c18f6892b7592ccc1594fda44668d14d8fc44
Andrea Pappacoda <andrea@pappacoda.it>
2021-04-05 11:34:17 +0200

    Add Meson clang-format target Since Meson doesn't have a way of specifying the working directory of a command, I had to modify the script to make it cd into the source root when ran from Meson.
    You can run the format target with meson compile -C builddir format or
    ninja -C builddir format

--- tools/format.sh
+++ tools/format.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -eu
+
+if [ ! -z ${MESON_SOURCE_ROOT+x} ]; then cd "${MESON_SOURCE_ROOT}"; fi
 
 find_files() {
     git ls-files --cached --exclude-standard --others | grep -E '\.(cc|cpp|h)$'
