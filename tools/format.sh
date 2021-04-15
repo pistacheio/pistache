8805bdc9e4e6e9fe1f30da7509d6e55f6285353b
Andrea Pappacoda <andrea@pappacoda.it>
2021-04-15 12:22:14 +0200

    Remove custom format target in Meson I've just found out that Meson automatically sets up a clang-format target when clang-format is found on the system, so there's no need to manually add that target.
    It can be ran with ninja clang-format

--- tools/format.sh
+++ tools/format.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-if [ ! -z ${MESON_SOURCE_ROOT+x} ]; then cd "${MESON_SOURCE_ROOT}"; fi
-
 find_files() {
     git ls-files --cached --exclude-standard --others | grep -E '\.(cc|cpp|h)$'
 }
