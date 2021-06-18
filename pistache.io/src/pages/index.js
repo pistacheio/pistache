c6d5ee97598b2996100a6e735d98a3b482e2d148
Andrea Pappacoda <andrea@pappacoda.it>
2021-06-18 23:03:31 +0200

    Clean upgrade to Docusaurus 2.0.0-beta.1

--- pistache.io/src/pages/index.js
+++ pistache.io/src/pages/index.js
@@ -3,104 +3,42 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
 
-const features = [
-  {
-    title: 'Modern API',
-    imageUrl: 'img/undraw_futuristic_interface.svg',
-    description: (
-      <>
-        Written in pure C++17 and providing a low-level HTTP abstraction,
-        Pistache makes playing with its modern API fun and easy,
-        just take a look at the quickstart
-      </>
-    ),
-  },
-  {
-    title: 'What\'s in the box',
-    imageUrl: 'img/undraw_accept_request.svg',
-    description: (
-      <>
-        <ul>
-          <li>A multi-threaded HTTP server to build your APIs</li>
-          <li>An asynchronous HTTP client to request APIs</li>
-          <li>An HTTP router to dispatch requests to C++ functions</li>
-          <li>A REST description DSL to easily define your APIs</li>
-          <li>Type-safe headers and MIME types implementation</li>
-        </ul>
-      </>
-    ),
-  },
-  {
-    title: 'Use it',
-    imageUrl: 'img/undraw_version_control.svg',
-    description: (
-      <>
-        <ul>
-          <li>Clone it on <a href="https://github.com/pistacheio/pistache">GitHub</a></li>
-          <li>Start with the <a href="docs/">quickstart</a></li>
-          <li>Read the full user's <a href="docs/http-handler">guide</a></li>
-          <li>Have issues with it? Fill an <a href="https://github.com/pistacheio/pistache/issues">issue</a></li>
-        </ul>
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
   return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className={clsx(
+              'button button--outline button--secondary button--lg',
+              styles.getStarted,
+            )}
+            to="/docs/">
+            Get Started
+          </Link>
         </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
+      </div>
+    </header>
   );
 }
 
-function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
+      //title={`Hello from ${siteConfig.title}`}
+      //description="Description will go into a meta tag in <head />"
+      >
+      <HomepageHeader />
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+        <HomepageFeatures />
       </main>
     </Layout>
   );
 }
-
-export default Home;
