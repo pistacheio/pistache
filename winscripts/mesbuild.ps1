da35e7dde02be0927a8cefcff60f56d592989a27
DMG <dgreatwood@gmail.com>
2024-08-04 17:05:06 -0700

    On Windows "meson setup" Runs To Completion
    Tweaked meson.build for Windows
    
    Added powershell (*.ps1) convenience scripts for build
    
    Note that most of the work here was build environment configuration,
    which will be documented/automated later.

---
+++ winscripts/mesbuild.ps1
@@ -0,0 +1,35 @@
+# PowerShell Script
+
+#
+# SPDX-FileCopyrightText: 2024 Duncan Greatwood
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Execute this script from the parent directory by invoking:
+#   winscripts/mesbuild.ps1
+
+# Use "dot source" to include another file
+. winscripts/messetdirvars.ps1
+
+if (($MESON_PREFIX_DIR) -and (-not (Test-Path -Path "$MESON_PREFIX_DIR"))) `
+    {mkdir "$MESON_PREFIX_DIR"}
+
+if (Test-Path -Path ".\$MESON_BUILD_DIR") {
+    rm -Recurse ".\$MESON_BUILD_DIR"
+#     Write-Host "Using existing build dir $MESON_BUILD_DIR"
+}
+# else {
+    Write-Host "Would use build dir $MESON_BUILD_DIR"
+    meson.exe setup "$MESON_BUILD_DIR" `
+    --buildtype=release `
+    -DPISTACHE_USE_SSL=true `
+    -DPISTACHE_BUILD_EXAMPLES=true `
+    -DPISTACHE_BUILD_TESTS=true `
+    -DPISTACHE_BUILD_DOCS=false `
+    -DPISTACHE_USE_CONTENT_ENCODING_DEFLATE=true `
+    --prefix="$MESON_PREFIX_DIR"
+# }
+
+# meson compile -C ${MESON_BUILD_DIR}
+
