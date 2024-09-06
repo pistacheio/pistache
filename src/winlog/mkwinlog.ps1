20f20b487fb133885956ce2b5c1f715b19c05e76
DMG <dgreatwood@gmail.com>
2024-09-06 08:29:18 -0700

    Use meson to Generate Windows Logging Files
    In src/meson.build, we now have custom targets that cause the
    generated files pist_winlog.rc, pist_winlog.h, pist_winlog.res, and
    pistachelog.dll to be created as part of the build process.
    
    src/winlog/mkwinlog.ps1 is modified so it exits before doing
    anything. It's not needed any more, but retained for now as
    documentation.

--- src/winlog/mkwinlog.ps1
+++ src/winlog/mkwinlog.ps1
@@ -1,5 +1,8 @@
 # https://kallanreed.com/2016/05/28/creating-an-etw-provider-step-by-step/
 
+Write-Host "Not needed any more - src/meson.build does it for us instead"
+return 0
+
 mc.exe -um -h "..\..\include\pistache\" -r ".\" pist_winlog.man
 rc.exe 'pist_winlog.rc'
 link.exe  /dll /noentry /machine:x64 pist_winlog.res /OUT:'C:\Program Files\pistache_distribution\pistachelog.dll'
