27490dd908366e582824ba649798347ce15e15bc
Jiankun Yu <jiankyu@ebay.com>
2025-05-05 19:34:09 -0700

    code style: fix alignment issue

--- include/pistache/config.h
+++ include/pistache/config.h
@@ -15,11 +15,11 @@
 // Allow compile-time overload
 namespace Pistache::Const
 {
-    static constexpr size_t MaxBacklog     = 128;
-    static constexpr size_t MaxEvents      = 1024;
-    static constexpr size_t MaxBuffer      = 4096;
+    static constexpr size_t MaxBacklog       = 128;
+    static constexpr size_t MaxEvents        = 1024;
+    static constexpr size_t MaxBuffer        = 4096;
     static constexpr size_t DefaultAcceptors = 1;
-    static constexpr size_t DefaultWorkers = 1;
+    static constexpr size_t DefaultWorkers   = 1;
 
     static constexpr size_t DefaultTimerPoolSize = 128;
 
